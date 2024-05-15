7e2be5b4a2411154204d6ed2e5e145980a069e5c
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-05-15 21:58:29 +0600

    chore: Improve number input styling and functionality

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -5,6 +5,8 @@ import React from "react"
 export default function Calculator({ service = "bkash" }) {
   const [value, setValue] = React.useState("")
 
+  const crossVisible = value
+
   const calculateBkashFee = (amount: number, type: string = "regular") => {
     // calculate 1.85% of the amount round to 2 decimal places
     const regularFee = 0.0185
@@ -33,30 +35,34 @@ export default function Calculator({ service = "bkash" }) {
 
   return (
     <div className='my-4'>
-      <form onSubmit={handleFormSubmit} className='flex gap-2'>
-        <section className='relative'>
+      <form onSubmit={handleFormSubmit} className='flex gap-2 items-center'>
+        <section className='relative flex-1'>
           <input
-            className=' border w-full text-3xl p-2'
+            className='border w-full text-3xl p-2 rounded-md '
             type='number'
             name='amount'
+            id='amount'
             onChange={(e) => setValue(e.target.value)}
             value={value}
             autoFocus
             tabIndex={1}
           />
-          <button
-            className='absolute right-[10px] top-[8px] bg-[#efefef] hover:bg-[#ddd] transition rounded-full flex items-center justify-center h-[40px] w-[40px] text-[32px] 
-            leading-none
-            '
-            type='button'
-            tabIndex={3}
-          >
-            &times;
-          </button>
+          {crossVisible && (
+            <button
+              onClick={(e) => setValue("")}
+              className='absolute right-[10px] top-[7px] bg-[#efefef] hover:bg-[#ddd] transition rounded-full flex items-center justify-center h-[40px] w-[40px] text-[32px] 
+              leading-none
+              '
+              type='button'
+              tabIndex={3}
+            >
+              &times;
+            </button>
+          )}
         </section>
-        <section>
+        <section className=''>
           <button
-            className='block bg-accent rounded-md px-4 py-2 font-bold'
+            className='block bg-accent rounded-md text-3xl px-4 py-2 '
             type='submit'
             tabIndex={2}
           >
