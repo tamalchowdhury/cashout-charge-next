a00ef60d850415482e293630c4306ea6c98599c8
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-05-15 16:39:34 +0600

    initial bare basic nextjs app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        accent: "#9BCF53",
+        background: "#e2e8f0"
+      }
     },
   },
   plugins: [],
